fe0648d28732dee62122517c44b2fe97ea3797e3
Peizhao Li <peizhaol@linux-15.andrew.cmu.edu>
2022-03-11 15:52:39 -0500

    fixed some minor bugs in connection, need to do more tests

--- content_server.py
+++ content_server.py
@@ -15,7 +15,7 @@ from datetime import datetime
 DEFAULT_PORT = 18346
 port = DEFAULT_PORT
 BUFSIZE = 1024
-global self_uuid, self_name, self_backend_port, self_hostname
+global self_uuid, self_name, self_backendport, self_hostname
 # uuid: distance map
 uuid_distance_map = dict()
 
@@ -36,6 +36,8 @@ def parse_conf(path):
     conf_file = open(path, "r")
     lines = conf_file.readlines()
     count_line = 0
+    peer_count = None
+    peer_nodes = None
     for line in lines:
         count_line += 1
         split_result = line.split(" = ")
@@ -44,12 +46,14 @@ def parse_conf(path):
         elif split_result[0] == "name":
             name = split_result[1].strip()
         elif split_result[0] == "backend_port":
-            backend_port = split_result[1].strip()
+            backend_port = int(split_result[1].strip())
         elif split_result[0] == "peer_count":
             peer_count = split_result[1].strip()
     if count_line >= 4:
         peer_nodes = lines[4:]
-    return (uuid, name, port, peer_count, peer_nodes)
+    if peer_count == None:
+        return (uuid, name, backend_port)
+    return (uuid, name, backend_port, peer_count, peer_nodes)
 
 # @brief used for "uuid" command
 # @param current node's uuid
@@ -72,6 +76,7 @@ def client_handle(srv):
     while True:
         msg_addr = srv.recvfrom(BUFSIZE)
         message_handle_thread = threading.Thread(target = message_handle, args = (msg_addr, srv))
+        message_handle_thread.daemon = True
         message_handle_thread.start()
 
 
@@ -140,15 +145,14 @@ def print_active_neighbors():
 # @brief Handle keepalive signal from received msg, if it is a new neighbor, add it to map
 # @param msg message sent from other content_server which may contain Alive_uuid_timestamps_hostname_backendport_metric
 def keepalive_handle(msg):
-    print(uuid_node_map)
     msg = msg.split("_")
     if msg[0] != "Alive":
         return
     uuid = msg[1]
     node_timestamp = float(msg[2])
     hostname = msg[3]
-    backend_port = msg[4]
-    metric = msg[5]
+    backend_port = int(msg[4])
+    metric = int(msg[5])
 
     # Update corresponding node's timestamp if it is in the 
     if uuid in uuid_node_map:
@@ -156,7 +160,7 @@ def keepalive_handle(msg):
         if tmp_node.timestamp <= node_timestamp:
             tmp_node.set_timestamp(node_timestamp)
     else:
-        print("keepalive_handle(): Node not in neighbors map---" + uuid + ", Need to add it")
+        # print("keepalive_handle(): Node not in neighbors map---" + uuid + ", Need to add it")
         new_node = Node(uuid, hostname, backend_port, metric)
         new_node.set_timestamp(node_timestamp)
         uuid_node_map[uuid] = new_node
@@ -167,24 +171,37 @@ def keepalive_handle(msg):
 # @brief Send keepalive signal to neighbor nodes
 # @param srv server socket used to send messages
 def send_keepalive(srv):
+    keepalive_thread = threading.Timer(5, send_keepalive, args = (s, ))
+    keepalive_thread.daemon = True
+    keepalive_thread.start()
     # Iterate through the whole neighbors map to find out stale nodes
+    cur_timestamp = datetime.timestamp(datetime.now())
+    # print("send keepalive: " + str(cur_timestamp))
+    deleted = set()
     for uuid in uuid_node_map:
         tmp_node = uuid_node_map[uuid]
-        if (cur_timestamp - tmp_node.timestamp > 15.0):
-            uuid_node_map.delete(uuid)
-            uuid_distance_map.delete(uuid)
+        if (cur_timestamp - tmp_node.timestamp > 15):
+            deleted.add(uuid)
+            print("Y")
+    
+    for uuid in deleted:
+        del uuid_node_map[uuid]
+        del uuid_distance_map[uuid]
+        print("deleted: " + uuid)
+    print("DELE: ", deleted)
 
     msg = "Alive"
     # Send to each neighbor node
     for uuid in uuid_node_map:
         tmp_node = uuid_node_map[uuid]
-        tmp_addr = tmp_node.host_name
+        tmp_addr = socket.gethostbyname(tmp_node.host_name)
 
         dt = datetime.now()
         timestamp = datetime.timestamp(dt)
         # protocol: Alive_uuid_timestamp_hostname_backendport_metric
-        msg += "_" + tmp_node.uuid + "_" + str(timestamp) + self_hostname + self_backendport + uuid_distance_map[uuid]
-        srv.sendto(msg.encode(), tmp_addr)
+        msg += "_" + self_uuid + "_" + str(timestamp) + "_" + self_hostname + "_" + str(self_backendport) + "_" + str(uuid_distance_map[uuid])
+        ADDR = (tmp_addr, tmp_node.backend_port)
+        srv.sendto(msg.encode(), ADDR)
         tmp_node.set_timestamp(timestamp)
     
 
@@ -233,7 +250,7 @@ if __name__ == "__main__":
     parsed_result = parse_conf(args.c)
     self_uuid = parsed_result[0]
     self_name = parsed_result[1]
-    self_backend_port = int(parsed_result[2])
+    self_backendport = int(parsed_result[2])
     if len(parsed_result) > 3:
         peer_count = int(parsed_result[3])
         peer_nodes = parsed_result[4]
@@ -246,16 +263,19 @@ if __name__ == "__main__":
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self_hostname = socket.gethostname()
 
-    # localip = socket.gethostbyname(socket.gethostname())
-    localip = "127.0.0.1"
+    localip = socket.gethostbyname(socket.gethostname())
+    # localip = "127.0.0.1"
     # bind socket
     try:
-        s.bind((localip, self_backend_port))
+        s.bind((localip, self_backendport))
     except socket.error as e:
         sys.exit(-1)
 
     current_thread = threading.Thread(target = client_handle, args = (s, ))
+    current_thread.daemon = True
     current_thread.start()
+
+    send_keepalive(s)
 
     # main loop
     while True:
@@ -265,7 +285,7 @@ if __name__ == "__main__":
         command_line_msg = input()
 
         if command_line_msg == "uuid":
-            print_uuid(uuid)
+            print_uuid(self_uuid)
         elif command_line_msg == "neighbors":
             print_active_neighbors()
         elif command_line_msg.split(" ")[0] == "addneighbor":
